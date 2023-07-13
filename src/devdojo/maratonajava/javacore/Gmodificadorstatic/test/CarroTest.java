a3873da3d798b36468df93e9d37acfd20d237080
Winkermann M <willmeschke@gmail.com>
2023-07-13 17:03:02 -0300

    Add mais um exemplo para atributos estaticos

--- src/devdojo/maratonajava/javacore/Gmodificadorstatic/test/CarroTest.java
+++ src/devdojo/maratonajava/javacore/Gmodificadorstatic/test/CarroTest.java
@@ -16,8 +16,22 @@ public class CarroTest {
         System.out.println("//////////////////////////////////////");
         System.out.println("");
 
-        //Se colocarmos c1.setVelocidadeLimite(150) todos os objetos serão afetados,
+        // Se atribuirmos Carro.velocidadeLimite = 180 siginifica que estamos alterando o valor de todos
+        // os objetos instanciados, já que se refere ao valor da classe
+        Carro.velocidadeLimite = 150;
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+
+        System.out.println("");
+        System.out.println("//////////////////////////////////////");
+        System.out.println("");
+
+        //A mesma coisa acontece se modificarmos diretamente em um objeto ja instanciado. Em
+        // em c1.setVelocidadeLimite(150) todos os objetos serão afetados,
         // uma vez que esse valor se refere à classe
+
         c1.setVelocidadeLimite(180);
         c1.imprime();
         c2.imprime();
